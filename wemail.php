cba69dabe8ee0b0878923abc69d51873db5e3c70
Edi Amin <to.ediamin@gmail.com>
2017-08-06 10:30:03 +0600

    Initial commit

---
+++ wemail.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: weMail
+ * Description: weMail plugin description...
+ * Plugin URI: https://wedevs.com/wemail/
+ * Author: weDevs
+ * Author URI: https://wedevs.com/
+ * Version: 0.0.1
+ * License: GPL-3.0
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wemail
+ * Domain Path: /languages
+ */
+/**
+ * Copyright (c) 2017 weDevs LLC (email: info@wedevs.com). All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * **********************************************************************
+ */
