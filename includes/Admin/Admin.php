3bd9cc9c333433e7f06950876155ed3ff41221d3
Tareq Hasan <tareq1988@gmail.com>
2020-08-19 11:04:53 +0800

    Redirect to setup page after plugin installation

--- includes/Admin/Admin.php
+++ includes/Admin/Admin.php
@@ -12,6 +12,7 @@ class Admin {
 
     public function __construct() {
         // $this->add_action( 'admin_init', 'remove_admin_notice' );
+        $this->add_action( 'admin_init', 'redirect_after_activation', 9999 );
 
         $this->includes();
     }
@@ -28,6 +29,30 @@ class Admin {
 
     public function remove_admin_notice() {
         remove_all_actions( 'admin_notices' );
+    }
+
+    /**
+     * Redirect to wemail setup page after plugin installation
+     *
+     * If the setup isn't done, it'll be redirected to the wemail page,
+     * also that redirects to the authentication page.
+     *
+     * @return void
+     */
+    public function redirect_after_activation() {
+        if ( ! get_transient( 'wemail_activation_redirect' ) || wemail()->user->api_key ) {
+            return;
+        }
+
+        delete_transient( 'wemail_activation_redirect' );
+
+        // Only do this for single site installs.
+        if ( is_network_admin() || isset( $_GET['activate-multi'] ) ) {
+            return;
+        }
+
+        wp_safe_redirect( admin_url( 'admin.php?page=wemail' ) );
+        exit;
     }
 
 }
--- includes/Install.php
+++ includes/Install.php
@@ -4,9 +4,11 @@ namespace WeDevs\WeMail;
 class Install {
 
     public static function install() {
-        $api_key = get_user_meta( get_current_user_id(), 'wemail_api_key', true );
-        $api = apply_filters( 'wemail_api_url', 'https://api.getwemail.io/v1' );
+        $api_key    = get_user_meta( get_current_user_id(), 'wemail_api_key', true );
+        $api        = apply_filters( 'wemail_api_url', 'https://api.getwemail.io/v1' );
         $wemail_api = untrailingslashit( $api );
+
+        set_transient( 'wemail_activation_redirect', true, 30 );
 
         wp_remote_post( $wemail_api . '/site/update-activation-status', [
             'headers'     => [
