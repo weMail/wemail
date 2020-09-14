9c08ddb66b8c8980a1d9fbc9809c0f1c07925a3a
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2020-09-14 11:49:40 +0600

    fix phpcs issues

--- includes/Core/Mail/WeMailMailer54.php
+++ includes/Core/Mail/WeMailMailer54.php
@@ -13,20 +13,22 @@ class WeMailMailer54 extends PHPMailer {
      * @throws \phpmailerException
      */
     public function send() {
-        $response = wemail()->api->emails()->transactional()->post( array(
-            'to' => $this->formatEmailAddress($this->phpmailer->getToAddresses()),
-            'subject' => $this->phpmailer->Subject,
-            'message' => $this->phpmailer->Body,
-            'type' => $this->phpmailer->ContentType,
-            'reply_to' => $this->phpmailer->getReplyToAddresses(),
-            'attachments' => $this->formatAttachments( $this->phpmailer->getAttachments() )
-        ) );
+        $response = wemail()->api->emails()->transactional()->post(
+            array(
+                'to' => $this->formatEmailAddress( $this->phpmailer->getToAddresses() ),
+                'subject' => $this->phpmailer->Subject,
+                'message' => $this->phpmailer->Body,
+                'type' => $this->phpmailer->ContentType,
+                'reply_to' => $this->phpmailer->getReplyToAddresses(),
+                'attachments' => $this->formatAttachments( $this->phpmailer->getAttachments() ),
+            )
+        );
 
         if ( is_wp_error( $response ) ) {
             throw new \phpmailerException( $response->get_error_message() );
         }
 
-        if ( isset( $response['success'] ) && ( $response['success'] != 'true' || $response['success'] != 1 ) ) {
+        if ( isset( $response['success'] ) && ! wemail_validate_boolean( $response['success'] ) ) {
             throw new \phpmailerException( 'Could not send transactional email' );
         }
 
--- includes/Core/Mail/WeMailMailer55.php
+++ includes/Core/Mail/WeMailMailer55.php
@@ -14,20 +14,22 @@ class WeMailMailer55 extends PHPMailer {
      * @throws PHPMailerException
      */
     public function send() {
-        $response = wemail()->api->emails()->transactional()->post( array(
-            'to' => $this->formatEmailAddress( $this->phpmailer->getToAddresses() ),
-            'subject' => $this->phpmailer->Subject,
-            'message' => $this->phpmailer->Body,
-            'type' => $this->phpmailer->ContentType,
-            'reply_to' => $this->phpmailer->getReplyToAddresses(),
-            'attachments' => $this->formatAttachments( $this->phpmailer->getAttachments() )
-        ) );
+        $response = wemail()->api->emails()->transactional()->post(
+            array(
+                'to' => $this->formatEmailAddress( $this->phpmailer->getToAddresses() ),
+                'subject' => $this->phpmailer->Subject,
+                'message' => $this->phpmailer->Body,
+                'type' => $this->phpmailer->ContentType,
+                'reply_to' => $this->phpmailer->getReplyToAddresses(),
+                'attachments' => $this->formatAttachments( $this->phpmailer->getAttachments() ),
+            )
+        );
 
         if ( is_wp_error( $response ) ) {
             throw new PHPMailerException( $response->get_error_message() );
         }
 
-        if ( isset( $response['success'] ) && ( $response['success'] != 'true' || $response['success'] != 1 ) ) {
+        if ( isset( $response['success'] ) && ! wemail_validate_boolean( $response['success'] ) ) {
             throw new PHPMailerException( 'Could not send transactional email' );
         }
 
