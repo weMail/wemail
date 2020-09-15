c496b80f56b24d60ec3d27443d55ea25f38017bb
Md. Emtiaz Zahid <emtiazzahid@gmail.com>
2020-09-15 12:09:09 +0600

    class for ping add

--- includes/Rest/Help/Help.php
+++ includes/Rest/Help/Help.php
@@ -2,6 +2,7 @@
 
 namespace WeDevs\WeMail\Rest\Help;
 
+use WeDevs\WeMail\Core\Help\Services\PingService;
 use WeDevs\WeMail\RestController;
 use WeMail\WPInfo\SystemInfo;
 use WP_REST_Server;
@@ -11,11 +12,27 @@ class Help extends RestController {
     public $rest_base = '/help';
 
     public function register_routes() {
-        register_rest_route( $this->namespace, $this->rest_base . '/system_info', [
+        register_rest_route( $this->namespace, $this->rest_base . '/system-info', [
             [
                 'methods'             => WP_REST_Server::READABLE,
                 'permission_callback' => [ $this, 'permission' ],
                 'callback'            => [ $this, 'index' ],
+            ]
+        ] );
+
+        register_rest_route( $this->namespace, $this->rest_base . '/tools/ping/send', [
+            [
+                'methods'             => WP_REST_Server::CREATABLE,
+                'permission_callback' => [ $this, 'permission' ],
+                'callback'            => [ $this, 'send_ping' ],
+            ]
+        ] );
+
+        register_rest_route( $this->namespace, $this->rest_base . '/tools/ping/receive', [
+            [
+                'methods'             => WP_REST_Server::CREATABLE,
+                'permission_callback' => [ $this, 'permission' ],
+                'callback'            => [ $this, 'receive_ping' ],
             ]
         ] );
     }
@@ -36,10 +53,25 @@ class Help extends RestController {
         $system = new SystemInfo();
 
         return rest_ensure_response([
-            'data' => [
-                $system->allInfo()
-            ]
+            'data' => $system->allInfo()
         ]);
     }
+
+    public function send_ping($request) {
+        $ping = new PingService();
+
+        $callback_url = get_rest_url() . $this->namespace . $this->rest_base . '/tools/ping/receive';
+
+        return rest_ensure_response($ping->request_send($request, $callback_url));
+    }
+
+    public function receive_ping($request) {
+        $ping = new PingService();
+
+        return rest_ensure_response($ping->request_receive($request));
+    }
+
+
+
 
 }
