5540f8ac7c7a9946a084e9f646290ca6485caef8
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2020-09-22 10:40:02 +0600

    Feature Fluent Form Integration (#167)
    * added fluent form integration
    
    * cast id to int

--- includes/Core/Form/Integrations/Hooks.php
+++ includes/Core/Form/Integrations/Hooks.php
@@ -25,6 +25,7 @@ class Hooks {
         add_action( 'caldera_forms_submit_complete', [ CalderaForms::instance(), 'submit' ], 999, 4 );
         add_action( 'weforms_entry_submission', [ Weforms::instance(), 'submit' ], 999, 4 );
         add_action( 'nf_save_sub', [ NinjaForms::instance(), 'submit' ], 999, 1 );
+        add_action( 'fluentform_before_insert_submission', [ FluentForms::instance(), 'submit' ], 999, 1 );
         add_action( 'happyforms_submission_success', [ HappyForms::instance(), 'submit' ], 999, 1 );
         add_action( 'frm_after_entry_processed', [ FormidableForms::instance(), 'submit' ], 999, 1 );
     }
