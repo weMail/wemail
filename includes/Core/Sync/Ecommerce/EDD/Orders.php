19fb37d9c65487cc458a0e179ff2976a3fe4d903
Md. Emtiaz Zahid <emtiazzahid@gmail.com>
2020-09-24 18:53:09 +0600

    edd orders api

--- includes/Core/Sync/Ecommerce/EDD/Orders.php
+++ includes/Core/Sync/Ecommerce/EDD/Orders.php
@@ -4,6 +4,7 @@ namespace WeDevs\WeMail\Core\Sync\Ecommerce\EDD;
 
 use WeDevs\WeMail\Core\Ecommerce\Requests\Orders as OrderRequest;
 use WeDevs\WeMail\Traits\Hooker;
+use WeDevs\WeMail\Core\Ecommerce\EDD\EDDOrders;
 
 class Orders {
 
@@ -42,26 +43,10 @@ class Orders {
             return;
         }
 
-        $payment_meta = edd_get_payment_meta( $payment_id );
-        $payment = new \EDD_Payment($payment_id);
-
-        $data = [
-            'source'               => $this->source,
-            'id'                   => $payment_id,
-            'parent_id'            => '',
-            'customer'             => $this->getCustomerInfo( $payment_meta['user_info'] ),
-            'status'               => edd_get_payment_status( $payment_id ),
-            'currency'             => $payment_meta['currency'],
-            'total'                => $payment->total,
-            'payment_method_title' => edd_get_payment_gateway($payment_id),
-            'date_created'         => date('Y-m-d H:m:s', strtotime($payment_meta['date'])),
-            'date_completed'       => date('Y-m-d H:m:s', strtotime($payment->completed_date)),
-            'permalink'            => get_permalink( $payment_id ),
-            'products'             => $this->get_ordered_products( $payment_meta['cart_details'] ),
-        ];
+        $eddOrders = new EDDOrders();
 
         $this->order_request->received(
-            $data,
+            $eddOrders->get( $payment_id ),
             $this->source
         );
     }
@@ -84,38 +69,5 @@ class Orders {
         ];
 
         $this->order_request->statusUpdated( $param, $this->source );
-    }
-
-    private function getCustomerInfo( $user ) {
-        return [
-            'wp_user_id'      => $user['id'] ?: '',
-            'first_name'      => $user['first_name'] ?: '',
-            'last_name'       => $user['last_name'] ?: '',
-            'email'           => $user['email'] ?: '',
-            'phone'           => '',
-            'address_1'       => $user['address'],
-            'address_2'       => '',
-            'city'            => '',
-            'state'           => '',
-            'postcode'        => '',
-            'country'         => ''
-        ];
-    }
-
-    private function get_ordered_products( $cart_details ) {
-        foreach($cart_details as $cart_item) {
-            $download = new \EDD_Download( $cart_item['id'] );
-
-            $products[] = [
-                'id'           => $download->ID,
-                'source'       => $this->source,
-                'name'         => $download->post_title,
-                'slug'         => $download->post_name,
-                'total'        => $cart_item['subtotal'],
-                'quantity'     => $cart_item['quantity'],
-            ];
-        }
-
-        return $products;
     }
 }
