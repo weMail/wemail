fc487b3337b9a9b0f719cd1af54d6da9af74ba9a
Md. Emtiaz Zahid <emtiazzahid@gmail.com>
2021-01-26 12:26:00 +0600

    revenue calculate feature

--- includes/Core/Ecommerce/Requests/Orders.php
+++ includes/Core/Ecommerce/Requests/Orders.php
@@ -8,12 +8,14 @@ class Orders {
 
     use Singleton;
 
+    private $revenue_cookie = 'wemail_campaign_revenue';
+
     /**
      * @param $payload
      * @param $source
      */
     public function received( $payload, $source ) {
-        wemail()->api->ecommerce()->orders()->post(
+        $response = wemail()->api->ecommerce()->orders()->post(
             [
                 'source'               => $source,
                 'id'                   => $payload['id'],
@@ -27,8 +29,11 @@ class Orders {
                 'date_completed'       => $payload['date_completed'],
                 'permalink'            => $payload['permalink'],
                 'products'             => $payload['products'],
+                'campaign_id'          => $this->campaign_id(),
             ]
         );
+
+        $this->clear_revenue_cookie($response, $payload['status']);
     }
 
     /**
@@ -36,12 +41,36 @@ class Orders {
      * @param $source
      */
     public function statusUpdated( $payload, $source ) {
-        wemail()->api->ecommerce()->order_status()->put(
+        $response = wemail()->api->ecommerce()->order_status()->put(
             [
-                'source'   => $source,
-                'order_id' => $payload['order_id'],
-                'status'   => $payload['status'],
+                'source'        => $source,
+                'order_id'      => $payload['order_id'],
+                'status'        => $payload['status'],
+                'campaign_id'   => $this->campaign_id(),
             ]
         );
+
+        $this->clear_revenue_cookie($response, $payload['status']);
+    }
+
+    /**
+     * Pass campaign information to track campaign along with order info
+     */
+    protected function campaign_id() {
+        if ( isset( $_COOKIE[ $this->revenue_cookie ] ) ) {
+            return $_COOKIE[ $this->revenue_cookie ];
+        }
+
+        return;
+    }
+
+    protected function clear_revenue_cookie($response, $status) {
+        if ( $status === 'completed' && isset($response['success']) && $response['success'] == true) {
+            unset( $_COOKIE[ $this->revenue_cookie ] );
+            setcookie($this->revenue_cookie, null, -1, '/');
+            return;
+        }
+
+        return $response;
     }
 }
---
+++ includes/Core/Sync/Ecommerce/RevenueTrack.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WeDevs\WeMail\Core\Sync\Ecommerce;
+
+use WeDevs\WeMail\Traits\Hooker;
+
+class RevenueTrack {
+
+    use Hooker;
+
+    /**
+     * Class constructor
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->add_action( 'template_redirect', 'wemail_check_revenue_calculation_cookie' );
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function wemail_check_revenue_calculation_cookie() {
+        if ( ! isset( $_GET['campaign_id'] ) ) {
+            return true;
+        }
+
+        if ( ! class_exists( 'woocommerce' ) && ! class_exists( 'Easy_Digital_Downloads' ) ) {
+            return true;
+        }
+
+        setcookie( 'wemail_campaign_revenue', $_GET['campaign_id'], strtotime( '+2 day' ), '/' );
+
+        return true;
+    }
+}
