48d55ff6366eeb3b43544927f26bbc29533569c8
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2021-09-21 15:45:40 +0600

    fix phpcs issue

--- includes/Admin/GutenbergBlock.php
+++ includes/Admin/GutenbergBlock.php
@@ -9,20 +9,22 @@ class GutenbergBlock {
     use Singleton, Hooker;
 
     public function boot() {
-        $this->add_action('init', 'register');
+        $this->add_action( 'init', 'register' );
         $this->add_action( 'enqueue_block_editor_assets', 'export_forms' );
     }
 
     public function register() {
-        $blockMeta = require_once WEMAIL_PATH . '/assets/js/index.asset.php';
+        $block_meta = require_once WEMAIL_PATH . '/assets/js/index.asset.php';
 
-        wp_register_style('wemail-block-style', WEMAIL_ASSETS . '/css/gutenberg.css', [], WEMAIL_VERSION);
-        wp_register_script('wemail-block-script', WEMAIL_ASSETS . '/js/index.js', $blockMeta['dependencies'], $blockMeta['version']);
+        wp_register_style( 'wemail-block-style', WEMAIL_ASSETS . '/css/gutenberg.css', [], WEMAIL_VERSION );
+        wp_register_script( 'wemail-block-script', WEMAIL_ASSETS . '/js/index.js', $block_meta['dependencies'], $block_meta['version'] );
 
-        register_block_type('wemail/forms', [
-            'editor_script' => 'wemail-block-script',
-            'editor_style' => 'wemail-block-style'
-        ]);
+        register_block_type(
+            'wemail/forms', [
+				'editor_script' => 'wemail-block-script',
+				'editor_style' => 'wemail-block-style',
+			]
+        );
     }
 
     public function export_forms() {
