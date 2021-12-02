b764c85e6054ad41adf56aaabdb4dc18b3ec5233
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2021-12-02 10:41:16 +0600

    Added categories ID on product

--- includes/Rest/Resources/Ecommerce/WooCommerce/OrderItemResource.php
+++ includes/Rest/Resources/Ecommerce/WooCommerce/OrderItemResource.php
@@ -16,11 +16,12 @@ class OrderItemResource extends JsonResource {
         $product = $order_item->get_product();
 
         return [
-            'id'        => $order_item->get_id(),
+            'id'        => $order_item->get_product_id(),
             'name'      => $order_item->get_name(),
             'quantity'  => $order_item->get_quantity(),
             'total'     => floatval( $order_item->get_total() ),
             'thumbnail' => $product ? wp_get_attachment_image_url( $product->get_image_id() ) : null,
+            'categories' => $product ? $product->get_category_ids() : [],
             'permalink' => $product ? $product->get_permalink() : null,
         ];
 	}
