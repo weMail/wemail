c299376736eb9ffd232c88f4050cc17e914044d2
Sumaisa Mou <mousumaisa@gmail.com>
2022-03-10 09:56:48 +0600

    Add integration for Everest Forms.

---
+++ includes/Core/Form/Integrations/EverestForms.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace WeDevs\WeMail\Core\Form\Integrations;
+
+use WeDevs\WeMail\Core\Form\Integrations\AbstractIntegration;
+use WeDevs\WeMail\Traits\Singleton;
+use WP_Query;
+
+class EverestForms extends AbstractIntegration {
+
+    use Singleton;
+
+    /**
+     * Integration title
+     *
+     * @since 1.0.0
+     *
+     * @var string
+     */
+    public $title = 'Everest Forms';
+
+    /**
+     * Integration slug
+     *
+     * @since 1.0.0
+     *
+     * @var string
+     */
+    public $slug = 'everest_forms';
+
+    /**
+     * Execute right after making class instance
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function boot() {
+        if ( class_exists( 'EverestForms' ) ) {
+            $this->is_active = true;
+        }
+        $this->forms();
+    }
+
+    /**
+     * Get available forms
+     *
+     * @since 1.0.0
+     *
+     * @return array
+     */
+    public function forms() {
+        $forms = [];
+
+        $query = new WP_Query(
+            [
+                'post_type' => 'everest_form',
+                'post_per_page' => -1,
+            ]
+        );
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            global $post;
+
+            $forms[] = [
+                'id' => absint( $post->ID ),
+                'title' => $post->post_title,
+                'fields' => $this->get_fields($post),
+            ];
+        }
+
+        return $forms;
+    }
+
+    /**
+     * @param $post
+     * @return mixed
+     */
+    public function get_fields($post)
+    {
+        $fields = [];
+
+        $form_fields = json_decode($post->post_content)->form_fields;
+        $get_columns = get_object_vars($form_fields);
+        foreach ($get_columns as $get_column) {
+            $fields[] = [
+                'id' =>  $get_column->id,
+                'label' => $get_column->label,
+            ];
+        }
+
+        return $fields;
+    }
+
+    /**
+     * Capture submission data
+     *
+     * @param $data
+     */
+    public function submit( $data ) {
+        $this->data = $data;
+
+        add_action( 'everestforms_form_submit_after', [ $this, 'subscribe' ] );
+    }
+
+    /**
+     * Subscribe subscriber
+     *
+     * @param $form
+     */
+    public function subscribe( $form ) {
+        $form_ids = get_option( 'wemail_form_integration_everest_forms', [] );
+
+        if ( ! in_array( $form['ID'], $form_ids, true ) ) {
+            return;
+        }
+
+        $data = [
+            'id' => $form['ID'],
+            'data' => $this->data,
+        ];
+
+        if ( ! empty( $data['data'] ) ) {
+            wemail_set_owner_api_key();
+            wemail()->api->forms()->integrations( 'everest_forms' )->submit()->post( $data );
+        }
+    }
+
+
+}
