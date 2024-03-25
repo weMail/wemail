86351581f762b8ee993673b3331b2c45d65d4653
Sumaisa Habib Mou <54771731+sumaisa-mou@users.noreply.github.com>
2024-03-25 10:47:24 +0600

    Fix notice vulnerability issue.

--- includes/Admin/Notice.php
+++ includes/Admin/Notice.php
@@ -30,6 +30,12 @@ class Notice {
 
         wp_enqueue_style( 'wemail-admin-notice-style' );
         wp_enqueue_script( 'wemail-admin-notice-script' );
+
+        wp_localize_script(
+            'wemail-admin-notice-script', 'wemail_notice_nonce', array(
+                'nonce' => wp_create_nonce( 'wemail_dismiss_notice_nonce' ),
+            )
+        );
     }
 
     /**
@@ -48,7 +54,7 @@ class Notice {
                     <p><?php echo __( 'You are one step closer to use weMail. Connect your site to get started with weMail. With weMail, you can send marketing and transactional emails to your audience.', 'wemail' ); ?></p>
                 </div>
                 <div class="wemail-connect-notice-connect-button">
-                    <a class="button" href="<?php echo wemail()->wemail_app . '/connect?email=' . urlencode( wp_get_current_user()->user_email ) . '&site_name=' . urlencode( get_bloginfo( 'name' ) ) . '&site_url=' . urlencode( site_url( '/' ) ) . '&redirect_url=' . urlencode( admin_url( 'admin.php?page=wemail' ) ); ?>">Connect</a>
+                    <a class="button" href="<?php echo wemail()->wemail_app . '/connect?email=' . rawurlencode( wp_get_current_user()->user_email ) . '&site_name=' . rawurlencode( get_bloginfo( 'name' ) ) . '&site_url=' . rawurlencode( site_url( '/' ) ) . '&redirect_url=' . rawurlencode( admin_url( 'admin.php?page=wemail' ) ); ?>">Connect</a>
                 </div>
             </div>
         <?php
@@ -61,7 +67,9 @@ class Notice {
      */
     public function connect_notice() {
         if ( isset( $_GET['dismiss_connect_notice'] ) && (int) $_GET['dismiss_connect_notice'] === 1 ) {
-            update_option( 'wemail_site_connection_notice', 1 );
+            if ( isset( $_GET['wemail_dismiss_notice_nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_GET['wemail_dismiss_notice_nonce'] ) ), 'wemail_dismiss_notice_nonce' ) ) {
+                update_option( 'wemail_site_connection_notice', 1 );
+            }
         }
         if ( ! get_user_meta( get_current_user_id(), 'wemail_api_key', true ) && (int) get_option( 'wemail_site_connection_notice' ) !== 1 && ! ( isset( $_GET['page'] ) && $_GET['page'] === 'wemail' ) ) {
             add_action( 'admin_notices', [ $this, 'connect_notice_html' ] );
@@ -69,3 +77,4 @@ class Notice {
     }
 
 }
+
