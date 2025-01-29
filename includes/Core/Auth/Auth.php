51961b469e43f1dcaa7213c6ee6bcb4334dd1896
bdmehedi <bdmehedi92@gmail.com>
2025-01-29 17:23:14 +0600

    Site connection with existing brand

--- includes/Core/Auth/Auth.php
+++ includes/Core/Auth/Auth.php
@@ -10,6 +10,24 @@ class Auth {
     use Singleton;
 
     /**
+     * Create brand on SaaS and connect this site with the brand
+     *
+     * @since 1.14.10
+     *
+     * @return \WP_Error|array
+     */
+    public function brand( $request = '') {
+        $data = array_merge($this->getData(), [
+            'brand_name' => $request->get_param( 'name' ),
+            'brand_slug' => $request->get_param( 'slug' ),
+        ]);
+
+        $response = wemail()->api->onboarding()->wp()->brands()->send_json()->post( $data );
+
+        return $this->handleResponse( $response );
+    }
+
+    /**
      * Authenticate the site
      *
      * @since 1.0.0
@@ -17,6 +35,24 @@ class Auth {
      * @return \WP_Error|array
      */
     public function site( $request = '') {
+        $data = array_merge($this->getData(), [
+            'api_key' => $request->get_param( 'api_key' ),
+        ]);
+
+        $response = wemail()->api->onboarding()->wp()->siteConnect()->send_json()->post( $data );
+
+        return $this->handleResponse( $response );
+    }
+
+    /**
+     * Get data for the API
+     *
+     * @since 1.0.0
+     *
+     * @return array
+     */
+    public function getData()
+    {
         // $start_of_week = get_option( 'start_of_week', 1 );
         // $week_days = array( 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat' );
 
@@ -55,33 +91,29 @@ class Auth {
             'rest_url'          => untrailingslashit( get_rest_url( null, '/wemail/v1/auth/validate-me' ) ),
             'key'               => $key,
             'created_by'        => $user->data->user_email,
-            'brand_name'        => $request->get_param( 'name' ),
-            'brand_slug'              => $request->get_param( 'slug' ),
         );
 
-        $response = wemail()->api->onboarding()->wp()->brands()->send_json()->post( $data );
+        return $data;
+    }
 
+    /**
+     * Handle the response
+     *
+     * @since 1.14.10
+     *
+     * @param array $response
+     *
+     * @return \WP_Error|array
+     */
+    private function handleResponse($response)
+    {
         if ( is_wp_error( $response ) ) {
             $data = $response->get_error_data();
             return new WP_Error( 'failed_to_connect_wemail', $response->get_error_message(), $data );
         }
 
         if ( ! empty( $response['access_token'] ) ) {
-            update_option( 'wemail_site_slug', $response['data']['slug'] );
-            $available_roles = array( 'administrator', 'editor' );
-            update_option( 'wemail_accessible_roles', $available_roles );
-            update_option( 'wemail_api_key', $response['access_token'] );
-
-            wemail()->api->set_api_key( $response['access_token'] );
-
-            /**
-             * Action hook fires after a site is authenticated
-             *
-             * @since 1.0.0
-             *
-             * @param array $response weMail API response after site authentication
-             */
-            do_action( 'wemail_site_authenticated', $response );
+            $this->saveAuthData( $response );
 
             return $response;
         }
@@ -98,5 +130,31 @@ class Auth {
         }
 
         return new WP_Error( 'failed_to_connect_wemail', $message, $data );
+    }
+
+    /**
+     * Save the authentication data
+     *
+     * @since 1.14.10
+     *
+     * @param array $response
+     */
+    private function saveAuthData($response)
+    {
+        update_option( 'wemail_site_slug', $response['slug'] );
+        $available_roles = array( 'administrator', 'editor' );
+        update_option( 'wemail_accessible_roles', $available_roles );
+        update_option( 'wemail_api_key', $response['access_token'] );
+
+        wemail()->api->set_api_key( $response['access_token'] );
+
+        /**
+         * Action hook fires after a site is authenticated
+         *
+         * @since 1.0.0
+         *
+         * @param array $response weMail API response after site authentication
+         */
+        do_action( 'wemail_site_authenticated', $response );
     }
 }
